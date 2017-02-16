a526da764d8907fec016eabfbf58a75336070391
byron blocker and Emily Dokken <emily@>
2017-02-15 19:00:39 -0800

    Built working calcultor

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+//business logic:
+
 var add = function(number1, number2) {
 	return number1 + number2;
 };
@@ -14,13 +16,40 @@ var divide = function(number1, number2) {
   return number1 / number2;
 }
 
-var number1 = parseInt(prompt("Enter a number"));
-var number2 = parseInt(prompt("Enter another number"));
-
-var result = add(number1, number2);
+//var result = add(number1, number2);
 //var result = subtract(number1, number2);
 //var result = multiply(number1, number2);
 //var result = divide(number1, number2);
 
+// front end logic
 
-alert(result);
+	$(document).ready(function() {
+		$("form#add").submit(function(event) {
+			event.preventDefault();
+		  var number1 = parseInt($("#add1").val());
+		  var number2 = parseInt($("#add2").val());
+			var result = add(number1, number2);
+			$("#addOutput").text(result);
+		});
+		$("form#subtract").submit(function(event) {
+			event.preventDefault();
+			var number1 = parseInt($("#subtract1").val());
+			var number2 = parseInt($("#subtract2").val());
+			var result = subtract(number1, number2);
+			$("#subtractOutput").text(result);
+		});
+		$("form#multiply").submit(function(event) {
+			event.preventDefault();
+			var number1 = parseInt($("#multiply1").val());
+			var number2 = parseInt($("#multiply2").val());
+			var result = multiply(number1, number2);
+			$("#multiplyOutput").text(result);
+		});
+		$("form#divide").submit(function(event) {
+			event.preventDefault();
+			var number1 = parseInt($("#divide1").val());
+			var number2 = parseInt($("#divide2").val());
+			var result = divide(number1, number2);
+			$("#divideOutput").text(result);
+		});
+	});
